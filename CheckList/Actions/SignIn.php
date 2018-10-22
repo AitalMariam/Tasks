4fa0a9956a108bc55f3a1cc8bb7d06f2d2a7849f
Mariam Ait Al <Aital_1998@outlook.com>
2018-10-22 01:46:12 +0100

    sign in action

---
+++ CheckList/Actions/SignIn.php
@@ -0,0 +1,21 @@
+<?php
+
+
+
+require_once '..\Operations\Users.php';
+
+session_start();
+if(isset($_POST['signinBtn']))
+{
+    //echo $_POST['email'].' '.$_POST['password'];
+    $operations = new Users();
+    $user = $operations->SignIn($_POST['email'],$_POST['password']);
+
+    if($user == 'ERR')
+        header('Location: ..\View\index.php');
+    else
+        header('Location: ..\View\home.php');
+}
+else
+    echo 'no';
+
---
+++ CheckList/Operations/Users.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: AitAl
+ * Date: 21/10/2018
+ * Time: 19:04
+ */
+
+require_once '..\Entities\User.php';
+require_once '..\Database\ConnectionInstance.php';
+
+class Users
+{
+    public $pdo = null;
+
+    public function __construct()
+    {
+        $this->pdo = Connection::getInstance();
+    }
+
+    public function SignIn($Email,$MotPasse)
+    {
+        $pdostate = $this->pdo->query("SELECT * FROM USER WHERE EMAIL ='$Email' and PASSWORD = '$MotPasse' LIMIT 1")->fetchObject('User');
+
+        // get and return user
+        if($pdostate == false)
+            return 'ERR';
+        else
+        {
+            $_SESSION["userConnected"] = $pdostate;
+            return 'pass';
+        }
+    }
+}
+
