2bf790e98b52aef968797841aa3a7bbed6abb361
Mariam Ait Al <Aital_1998@outlook.com>
2018-11-02 20:36:35 +0000

    Merge branch 'master' of https://github.com/AitalMariam/Tasks

--- checkList_V2/Index.php
+++ checkList_V2/Index.php
@@ -40,7 +40,12 @@
                             <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
                         </div>
                         <br>
-
+                        <div class="form-label-group">
+                            <select style="border-radius: 50px;" class="custom-select" name="role" id="role">
+                                <option value="user">User</option>
+                                <option value="admin">Admin</option>
+                            </select>
+                        </div>
                         <br>
                         <button name="sign_up" class="btn btn-lg btn-primary btn-block text-uppercase">Sign in</button>
                     </form>
@@ -75,12 +80,7 @@
                             <input type="password"  class="form-control" id="password" name="password_in" placeholder="Password" required>
                         </div>
                         <br>
-                        <div class="form-label-group">
-                            <select style="border-radius: 50px;" class="custom-select" name="role" id="role">
-                                <option value="user">User</option>
-                                <option value="admin">Admin</option>
-                            </select>
-                        </div>
+
                         <br>
                         <div class="custom-control custom-checkbox mb-3">
                             <input type="checkbox" class="custom-control-input" id="customCheck1">
